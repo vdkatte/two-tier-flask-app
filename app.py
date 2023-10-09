99ee7ea4b807f44a815d9f8f798882e165815e78
Vijay <89314816+vdkatte@users.noreply.github.com>
2023-10-09 12:10:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,21 +15,20 @@ mysql = MySQL(app)
 
 @app.route('/')
 def hello():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT message FROM messages')
-    messages = cur.fetchall()
-    cur.close()
+    with mysql.connection.cursor() as cur:
+        cur.execute('SELECT message FROM messages')
+        messages = cur.fetchall()
     return render_template('index.html', messages=messages)
 
 @app.route('/submit', methods=['POST'])
 def submit():
     new_message = request.form.get('new_message')
-    cur = mysql.connection.cursor()
-    cur.execute('INSERT INTO messages (message) VALUES (%s)', [new_message])
-    mysql.connection.commit()
-    cur.close()
+    with mysql.connection.cursor() as cur:
+        cur.execute('INSERT INTO messages (message) VALUES (%s)', [new_message])
+        mysql.connection.commit()
     return redirect(url_for('hello'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # Use a production-ready server like Gunicorn for deployment
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
